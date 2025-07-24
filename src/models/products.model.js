a95f0e0cb366935ad04e9fcc73692cb26a828791
Ailín Mercado <ailinnmercado@gmail.com>
2025-07-24 18:31:51 -0300

    Firebase

--- src/models/products.model.js
+++ src/models/products.model.js
@@ -1,6 +1,27 @@
+import { db } from "./firebase.js";
 
-export const getAllProducts = () => {
-    return [
-        {id:1, name: "Product 1"}
-    ]
+import {collection, getDocs } from "firebase/firestore";
+
+const productsCollection = collection(db, "products");
+
+export const getAllProducts = async () => {
+    try{
+        const snapshot = await getDocs(productsCollection);
+        const products = snapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+        }));
+        return products;
+    }
+    catch (error) {
+        console.error("Error en getAllProducts:", error);
+        throw error; // Propaga el error para manejarlo en el controlador.
+    }
+    
+    //return [
+    //    {id:1, name: "Product 1"}
+    //]
+    // En products.model.js
+console.log("Firestore db:", db); // Debería mostrar el objeto de Firestore
+console.log("getDocs function:", getDocs); // Debería mostrar [Function: getDocs]
 }
