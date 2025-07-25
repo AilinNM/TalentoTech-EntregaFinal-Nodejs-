7820a3fdccef0dc57ffef19f9f0e6167bc1f1484
Ailín Mercado <ailinnmercado@gmail.com>
2025-07-24 21:15:37 -0300

    JWT

--- src/models/products.model.js
+++ src/models/products.model.js
@@ -1,6 +1,6 @@
 import { db } from "./firebase.js";
 
-import {collection, getDocs } from "firebase/firestore";
+import {collection, getDocs, doc, getDoc } from "firebase/firestore";
 
 const productsCollection = collection(db, "products");
 
@@ -18,10 +18,26 @@ export const getAllProducts = async () => {
         throw error; // Propaga el error para manejarlo en el controlador.
     }
     
+    
     //return [
     //    {id:1, name: "Product 1"}
     //]
     // En products.model.js
 console.log("Firestore db:", db); // Debería mostrar el objeto de Firestore
 console.log("getDocs function:", getDocs); // Debería mostrar [Function: getDocs]
+}
+
+export const getProductById = async (id) => {
+    try{
+        const docRef = doc(productsCollection, id);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()){
+            return {id: docSnap.id, ...docSnap.data()};
+        } else {
+            return null;
+        }
+    }
+    catch (error){
+        console.error(error);
+    }
 }
--- src/services/products.services.js
+++ src/services/products.services.js
@@ -4,3 +4,7 @@ import * as Model from '../models/products.model.js';
 export const getAllProducts = async () => {
     return await Model.getAllProducts();
 }
+
+export const getProductById = async (id) => {
+    return await Model.getProductById(id);
+};
